3a7f56d6097e2cf328f3379e7100805fc6325a20
SeagullOddy <shabbyacc@outlook.com>
2023-09-01 23:32:34 +0800

    fix up

--- bin/acl.ps1
+++ bin/acl.ps1
@@ -1,5 +1,8 @@
 # 真正执行脚本
 $grandpaPath = Split-Path -Parent $PSScriptRoot
-& ($grandpaPath + "\02acl\auto-campus-login\exe.ps1")
-& ($grandpaPath + "\02acl\\email-ip\exe.ps1")
 
+Set-Location ($grandpaPath + "\02acl\auto-campus-login\")
+py.exe exe.py
+
+Set-Location ($grandpaPath + "\02acl\email-ip\")
+pwsh.exe exe.ps1
